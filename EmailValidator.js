3bde38c54e115b1407f3e16aaed9961c8075d870
drshny03 <darshanyadav7049@gmail.com>
2025-03-04 13:54:57 +0530

    [Darshan] Add . ensure @ and validate the mandatory domain

--- EmailValidator.js
+++ EmailValidator.js
@@ -1,5 +1,5 @@
 //regex to validate email
-const regex = /^[a-zA-Z0-9]+/;
+const regex = /^[a-zA-Z0-9]+@[a-zA-z]+/;
 
 //function to validate email
 function validate(email){
@@ -8,3 +8,4 @@ function validate(email){
 
 //test
 console.log(validate("abc"));
+console.log(validate("abc@bridgelabz"));
